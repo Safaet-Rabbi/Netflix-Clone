4c278ab1bc34a07a7895618f5c7afc2bd86b322c
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-09-22 10:35:18 +0600

    Add files via upload

--- netflix/src/utils/constant.js
+++ netflix/src/utils/constant.js
@@ -11,7 +11,6 @@ export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing
 export const Popular_Movie = "https://api.themoviedb.org/3/movie/popular";
 export const Top_Rated_Movie = "https://api.themoviedb.org/3/movie/top_rated";
 export const Upcoming_Movie = "https://api.themoviedb.org/3/movie/upcoming";
-export const GOOGLE_CLIENT_ID = '473139808443-p392p62jlqoktd8ks61ckkr2lpl83t09.apps.googleusercontent.com';
 
 export const  SEARCH_MOVIE_URL="https://api.themoviedb.org/3/search/movie?query=";
 
