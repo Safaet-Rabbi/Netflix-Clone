3079639886b910c74b97c4c9f8e5d19d66567481
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-09-22 10:35:45 +0600

    Add files via upload

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
 import Body from "./components/Body";
 import { Toaster } from 'react-hot-toast'; 
 import MovieDialog from "./components/MovieDialog";
-
+ 
 function App() {
   return (
     <div>
