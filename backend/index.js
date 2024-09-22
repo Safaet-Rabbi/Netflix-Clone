6f6cfe3a0b8e39e15d7e5c79e54c523db11dab57
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-09-22 10:32:49 +0600

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,32 @@
+//step-1
+// const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
 import databaseConnection from "./utils/database.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/userRoute.js";
 import cors from "cors";
-import passport from "passport";
-import session from "express-session";
-import "./utils/passport.js"; // Import passport configuration
-
-dotenv.config({ path: ".env" });
 
 databaseConnection();
 
-const app = express();
+dotenv.config({
+    path:".env"
+})
 
-// Middlewares
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+//middlewares 
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-
-const allowedOrigins = ['http://localhost:3000', 'https://cosmic-starburst-73160f.netlify.app'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // This allows cookies and authentication data to be sent with requests
-}));
-
-
-// Session and Passport setup
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Routes
+const corsOptions = {
+    origin:'http://localhost:3000',
+    credentials:true
+}
+app.use(cors(corsOptions));
+ 
+// api
 app.use("/api/v1/user", userRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+app.listen(process.env.PORT,() => {
+    console.log(`Server listen at port ${process.env.PORT}`);
 });
